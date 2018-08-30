05ac1ddecc3fe844143de9aee49a35ceb64d5302
Josh <jkwroble@gmail.com>
2018-08-30 09:26:28 -0400

    Conditional tutorial

---
+++ Tutorial/_Practice/Lists/app.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    ingredients: ['meat', 'fruit', 'cookies'],
+    persons: [
+      {name: 'Max', age: 27, color: 'Red'},
+      {name: 'Anna', age: 'Unknown', color: 'Blue'}
+    ]
+  }
+});
